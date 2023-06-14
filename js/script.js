39ec041128ff6748e2f7055cec4cb6d38f911377
WallyssonSousa <wallysson.dev@gmail.com>
2023-06-14 16:15:31 -0300

    começando os cards

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Função para mostrar/ocultar o menu quando o ícone for clicado
+document.querySelector('.menu-icon').addEventListener('click', function() {
+  document.querySelector('nav.menu').classList.toggle('show');
+});
