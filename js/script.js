e1b9e3cf41e254ee1cb4a09df42e99114914dcea
WallyssonSousa <wallysson.dev@gmail.com>
2023-08-24 21:47:22 -0300

    começando modo dark

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@
 document.querySelector('.menu-icon').addEventListener('click', function() {
   document.querySelector('nav.menu').classList.toggle('show');
 });
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+  document.body.classList.toggle('dark')
+})
