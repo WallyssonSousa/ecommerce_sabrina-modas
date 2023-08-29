d7ad59525535db4835e34995da35118bbb51a344
WallyssonSousa <wallysson.dev@gmail.com>
2023-08-28 22:53:48 -0300

    Modo nortuno finalizado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-// Função para mostrar/ocultar o menu quando o ícone for clicado
-document.querySelector('.menu-icon').addEventListener('click', function() {
-  document.querySelector('nav.menu').classList.toggle('show');
-});
-
 const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () => {
